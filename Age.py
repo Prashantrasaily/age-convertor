ab1026ac8c0fc4a5b77746831d3b1f121d0d3225
Sita833 <72214892+Sita833@users.noreply.github.com>
2020-10-01 22:01:48 +0530

    made the code more flexible and readable
    I used comments and added the current_year variable.

--- Age.py
+++ Age.py
@@ -1,3 +1,5 @@
-birth_year =input("Birth year: ")
-age =2020 - int(birth_year)
-print(f" you are {age} years old ")
+# note:please update the current year variable to the year of accessing the code .
+current_year = 2020 # 2020 as the code is used in 2020
+birth_year =input("Birth year: ") # asks the user for birth year
+age =current_year - int(birth_year) # calculates the age
+print(f" you are {age} years old ") # prints the age in the terminal
