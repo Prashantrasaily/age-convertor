0b3eb73a7adceaa51adde58e0bae6664523d3995
Robin22545 <72149623+Robin22545@users.noreply.github.com>
2021-10-13 16:17:49 +0530

    C++ code to do the same job

--- Age.py
+++ Age.py
@@ -1,5 +1,11 @@
-# note:please update the current year variable to the year of accessing the code .
-current_year = 2020 # 2020 as the code is used in 2020
-birth_year =input("Birth year: ") # asks the user for birth year
-age =current_year - int(birth_year) # calculates the age
-print(f" you are {age} years old ") # prints the age in the terminal
+#include <iostream>
+using namespace std;
+int main()
+{
+    int BY;
+    int current_year = 2021;
+    cout << "Please enter your birth year\n";
+    cin >> BY;
+    cout << "Your age is " << current_year - BY << endl;
+    return 0;
+}
